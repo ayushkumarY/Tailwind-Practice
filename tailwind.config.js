14679392cae86431f8104b1ed644d3701d874dac
ayushkumarY <ayushkumar777y@gmail.com>
2024-01-26 14:59:43 +0530

    firstcommmit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#00008B',
+      },
+    },
+  },
+  plugins: [],
+};
